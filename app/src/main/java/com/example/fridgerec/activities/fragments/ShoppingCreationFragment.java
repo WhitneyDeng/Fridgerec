01ceddd45c8772ed28fcd74aba13525acec7fc2e
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-06-27 16:18:20 -0700

    save navigation from shopping creation to list

--- app/src/main/java/com/example/fridgerec/activities/fragments/ShoppingCreationFragment.java
+++ app/src/main/java/com/example/fridgerec/activities/fragments/ShoppingCreationFragment.java
@@ -2,7 +2,12 @@ package com.example.fridgerec.activities.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +21,8 @@ import com.example.fridgerec.R;
  * create an instance of this fragment.
  */
 public class ShoppingCreationFragment extends Fragment {
+  private Toolbar toolbar;
+  private NavController navController;
 
   public ShoppingCreationFragment() {
     // Required empty public constructor
@@ -26,5 +33,24 @@ public class ShoppingCreationFragment extends Fragment {
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_shopping_creation, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    navController = Navigation.findNavController(view);
+
+    toolbar = view.findViewById(R.id.toolbar);
+
+    toolbar.setOnMenuItemClickListener(item -> {
+      switch (item.getItemId()) {
+        case R.id.miSave:
+          //todo: save item
+          navController.navigate(R.id.action_shoppingCreationFragment_to_shoppingFragment);
+          return true;
+        default:
+          return false;
+      }
+    });
   }
 }
