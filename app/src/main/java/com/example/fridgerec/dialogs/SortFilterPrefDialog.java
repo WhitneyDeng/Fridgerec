5776a05ec6343c2090e1c3ebeec0c09ca65d54c8
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-08 17:45:46 -0700

    move return default date into catch clause

--- app/src/main/java/com/example/fridgerec/dialogs/SortFilterPrefDialog.java
+++ app/src/main/java/com/example/fridgerec/dialogs/SortFilterPrefDialog.java
@@ -166,6 +166,7 @@ public class SortFilterPrefDialog extends DialogFragment{
       Log.e(TAG, "unable to extract date from" + btnDatepicker.getText().toString());
       Toast.makeText(getContext(), "unselected date (default to today)", Toast.LENGTH_SHORT).show();
       e.printStackTrace();
+
       return new Date(System.currentTimeMillis());
     }
   }
