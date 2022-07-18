5f4a1e888113f4a4538f25aa96d8fc6b3e9b1ba3
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-18 14:27:15 -0700

    refactor shared functionality bn ShoppingFragment and InventoryFragment to CreationFragment

--- app/src/main/java/com/example/fridgerec/fragments/ShoppingFragment.java
+++ app/src/main/java/com/example/fridgerec/fragments/ShoppingFragment.java
@@ -7,31 +7,31 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fridgerec.R;
 import com.example.fridgerec.databinding.FragmentShoppingBinding;
+import com.example.fridgerec.model.viewmodel.InventoryViewModel;
+import com.example.fridgerec.model.viewmodel.ShoppingViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ShoppingFragment} factory method to
  * create an instance of this fragment.
  */
-public class ShoppingFragment extends Fragment {
-  private AppBarConfiguration appBarConfiguration;
-  private NavController navController;
+public class ShoppingFragment extends ListFragment {
   private FragmentShoppingBinding binding;
-
-
-  private PopupMenu popupMenu;
 
   public ShoppingFragment() {
     // Required empty public constructor
@@ -47,45 +47,20 @@ public class ShoppingFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    setupListFragment(new ViewModelProvider(requireActivity()).get(ShoppingViewModel.class), view);
+    model.enterReadMode();
 
-
-    navController = Navigation.findNavController(view);
-
-    setupToolbar();
-    onClickToolbarItem(view);
-
-    binding.fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Navigation.findNavController(view).navigate(R.id.action_shoppingFragment_to_shoppingCreationFragment);
-      }
-    });
+    setupToolbar(binding.toolbar);
+    onClickFab(binding.fab);
   }
 
-  private void setupToolbar() {
-    appBarConfiguration = new AppBarConfiguration.Builder(R.id.inventoryFragment, R.id.shoppingFragment, R.id.settingsFragment).build();
-    NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+  @Override
+  protected boolean onContextualMenuItemClicked(ActionMode mode, MenuItem item) {
+    return false;
   }
 
-  private void onClickToolbarItem(View view) {
-    binding.toolbar.setOnMenuItemClickListener(item -> {
-      switch (item.getItemId()) {
-        case R.id.miFilter:
-          showPopup(view.findViewById(R.id.miFilter), R.menu.menu_popup_filter);
-          return true;
-        case R.id.miSort:
-          showPopup(view.findViewById(R.id.miSort), R.menu.menu_popup_sort);
-          return true;
-        default:
-          return false;
-      }
-    });
-  }
-
-  private void showPopup(View view, @MenuRes int menu_popup) {
-    popupMenu = new PopupMenu(getActivity(), view);
-    MenuInflater inflater = popupMenu.getMenuInflater();
-    inflater.inflate(menu_popup, popupMenu.getMenu());
-    popupMenu.show();
+  @Override
+  protected void navigateToCreation() {
+    navController.navigate(R.id.action_shoppingFragment_to_shoppingCreationFragment);
   }
 }
