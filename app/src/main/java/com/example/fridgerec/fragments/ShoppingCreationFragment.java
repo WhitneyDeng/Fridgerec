56c56b0ef379673a1469ac8034a90e5cce97e40e
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-18 16:01:05 -0700

    implement add shopping item

--- app/src/main/java/com/example/fridgerec/fragments/ShoppingCreationFragment.java
+++ app/src/main/java/com/example/fridgerec/fragments/ShoppingCreationFragment.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,15 +14,19 @@ import android.view.ViewGroup;
 
 import com.example.fridgerec.R;
 import com.example.fridgerec.databinding.FragmentShoppingCreationBinding;
+import com.example.fridgerec.fragments.basefragments.CreationBaseFragment;
+import com.example.fridgerec.model.EntryItem;
+import com.example.fridgerec.model.Food;
+import com.example.fridgerec.model.viewmodel.ShoppingViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ShoppingCreationFragment} factory method to
  * create an instance of this fragment.
  */
-public class ShoppingCreationFragment extends Fragment {
-  private NavController navController;
-  private AppBarConfiguration appBarConfiguration;
+public class ShoppingCreationFragment extends CreationBaseFragment {
+  public static final String TAG = "ShoppingCreationFragment";
+
   private FragmentShoppingCreationBinding binding;
 
   public ShoppingCreationFragment() {
@@ -41,29 +43,41 @@ public class ShoppingCreationFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    setupBaseFragment(new ViewModelProvider(requireActivity()).get(ShoppingViewModel.class), view);
     navController = Navigation.findNavController(view);
 
+    if (Boolean.TRUE.equals(model.getInEditMode().getValue())) {
+      populateEntryItemDetail(model.getSelectedEntryItem());
+    }
 
-    setupToolbar();
-    onClickToolbarItem();
-
+    setupToolbar(binding.toolbar);
+    configExposedDropdownMenu(binding.actvFoodGroup, getResources().getStringArray(R.array.foodGroupStrings));
+    configExposedDropdownMenu(binding.actvAmountUnit, getResources().getStringArray(R.array.amountUnitStrings));
   }
 
-  private void setupToolbar() {
-    appBarConfiguration = new AppBarConfiguration.Builder(R.id.inventoryFragment, R.id.shoppingFragment, R.id.settingsFragment).build();
-    NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+  @Override
+  protected void populateEntryItemDetail(EntryItem entryItem) {
+    populateString(entryItem.getFood().getFoodName(), binding.tilFood);
+    populateString(entryItem.getFood().getFoodGroup(), binding.tilFoodGroup);
+    populateString(Integer.toString(entryItem.getAmount()), binding.tilAmount);
+    populateString(entryItem.getAmountUnit(), binding.tilAmountUnit);
   }
 
-  private void onClickToolbarItem() {
-    binding.toolbar.setOnMenuItemClickListener(item -> {
-      switch (item.getItemId()) {
-        case R.id.miSave:
-          //TODO: save item
-          navController.navigate(R.id.action_shoppingCreationFragment_to_shoppingFragment);
-          return true;
-        default:
-          return false;
-      }
-    });
+  @Override
+  protected EntryItem extractEntryItem() {
+    EntryItem entryItem = getEntryItem();
+
+    Food food = extractFood(binding.tilFood, binding.tilFoodGroup);
+    if (food == Food.DUMMY_FOOD) {
+      return EntryItem.DUMMY_ENTRY_ITEM;
+    }
+    entryItem.setFood(food);
+
+    return extractAmountInfo(entryItem, binding.tilAmount, binding.tilAmountUnit);
+  }
+
+  @Override
+  protected void navigateAway() {
+    navController.navigate(R.id.action_shoppingCreationFragment_to_shoppingFragment);
   }
 }
