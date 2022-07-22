7f95472e74f07a189b2263ce9fa9532cb602f3cc
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-22 13:25:43 -0700

    pass textview to API client => set API query result to adapter

--- app/src/main/java/com/example/fridgerec/SpoonacularClient.java
+++ app/src/main/java/com/example/fridgerec/SpoonacularClient.java
@@ -7,8 +7,11 @@ import android.widget.AutoCompleteTextView;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.fridgerec.interfaces.SetAutocompleteTextViewAdapterCallback;
+import com.example.fridgerec.model.Food;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 
 import okhttp3.Headers;
 
@@ -16,8 +19,7 @@ public class SpoonacularClient {
   public static final String TAG = "SpoonacularClient";
   public static final String BASE_PATH = "https://api.spoonacular.com";
 
-
-  public static void getAutocompleteSuggestions(Context context, String query, AutoCompleteTextView actv) {
+  public static void getAutocompleteSuggestions(Context context, String query, AutoCompleteTextView actv, SetAutocompleteTextViewAdapterCallback view) {
     String autocompleteEndptPath = "/food/ingredients/autocomplete";
 
     AsyncHttpClient client = new AsyncHttpClient();
@@ -25,14 +27,19 @@ public class SpoonacularClient {
     params.put("query", query);
     params.put("metaInformation", true);
 
-    // + "?apiKey=" + context.getString(R.string.spoonacular_api_key)
     client.get(BASE_PATH + autocompleteEndptPath, params, new JsonHttpResponseHandler() {
       @Override
       public void onSuccess(int statusCode, Headers headers, JSON json) {
         Log.i(TAG, "[autocomplete query onSuccess]");
 
-        JSONArray suggestions = json.jsonArray;
-        Log.i(TAG, "results" + suggestions.toString()); //testing
+        JSONArray results = json.jsonArray;
+        Log.i(TAG, "results" + results.toString()); //testing
+
+        try {
+          view.setAutocompleteTextViewAdapter(actv, Food.fromJsonArray(results));
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
 
         // TODO: extract food objects from result
         // TODO: attach new adapter
