525d834f36469f1cc6dcf7cb19376b34732ef824
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-22 14:33:11 -0700

    add & implement onSuggestionSelected() in fragments

--- app/src/main/java/com/example/fridgerec/fragments/InventoryCreationFragment.java
+++ app/src/main/java/com/example/fridgerec/fragments/InventoryCreationFragment.java
@@ -91,4 +91,14 @@ public class InventoryCreationFragment extends CreationBaseFragment {
   protected void navigateAway() {
     navController.navigate(R.id.action_inventoryCreationFragment_to_inventoryFragment);
   }
+
+  @Override
+  public void onSuggestionSelected(Food selectedFood) {
+    //TODO: populate food group & set dropdown to unclickable
+    populateTextInputLayout(selectedFood.getFoodGroup(), binding.tilFoodGroup);
+    binding.tilFoodGroup.setEnabled(false);
+    //TODO: create chip in food actv
+    //TODO: disable food actv
+    //TODO: save food to view model
+  }
 }
