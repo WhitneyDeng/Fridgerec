dc0f84a439a8974b29c33ec8ec03d208aff9c707
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-26 10:33:45 -0700

    delete User object

--- app/src/main/java/com/example/fridgerec/model/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.fridgerec.model;
-
-// saves column names of User Parse classes
-public class User {
-  public static final String KEY_NOTIFICATION_TIME = "notificationTime";
-  public static final String KEY_NOTIFICATION_SOURCE_DATE_OFFSET = "notificationSourceDateOffset";
-  public static final String KEY_NOTIFICATION_EXPIRE_DATE_OFFSET = "notificationExpireDateOffset";
-}
