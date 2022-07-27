312e7cd54216c0418d0e3132e8442a1b616537dc
WhitneyDeng <32890361+WhitneyDeng@users.noreply.github.com>
2022-07-27 10:10:59 -0700

    create notification channel

--- app/src/main/java/com/example/fridgerec/activities/MainActivity.java
+++ app/src/main/java/com/example/fridgerec/activities/MainActivity.java
@@ -8,10 +8,14 @@ import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.fridgerec.R;
+import com.example.fridgerec.ReminderReceiver;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.soloader.SoLoader;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -33,7 +37,20 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     setupBottomNav();
+    configNavController();
+    createNotificationChannel(ReminderReceiver.REMINDER_CHANNEL_NAME, ReminderReceiver.REMINDER_CHANNEL_ID);
+  }
 
+  private void createNotificationChannel(String channelName, String channelId) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      int importance = NotificationManager.IMPORTANCE_DEFAULT;
+      NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+      NotificationManager notificationManager = getSystemService(NotificationManager.class);
+      notificationManager.createNotificationChannel(channel);
+    }
+  }
+
+  private void configNavController() {
     navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
       @Override
       public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
